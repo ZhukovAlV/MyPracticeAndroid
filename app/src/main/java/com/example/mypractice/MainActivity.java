86dcad70eeaf6347fcfeef795c20540e9c12ec55
cooluser <avzhykov@gmail.com>
2023-01-14 23:27:32 +0600

    Добавлен слушатель на кнопку

--- app/src/main/java/com/example/mypractice/MainActivity.java
+++ app/src/main/java/com/example/mypractice/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.mypractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myText = findViewById(R.id.myText);
+    }
+
+    public void clickButton(View view) {
+        myText.setText("Новый текст");
     }
 }
