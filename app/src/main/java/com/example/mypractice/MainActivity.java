b3f481ae2ee8a40ff17ca29dc177dc513b7a7704
cooluser <avzhykov@gmail.com>
2023-01-15 18:35:56 +0600

    Итоги с группой 134

--- app/src/main/java/com/example/mypractice/MainActivity.java
+++ app/src/main/java/com/example/mypractice/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.mypractice;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textForView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textForView = findViewById(R.id.myText);
+    }
+
+    public void clickButton(View view) {
+        textForView.setText("Я молодец");
     }
 }
